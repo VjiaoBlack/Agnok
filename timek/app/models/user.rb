77d5f8de8a1072636fec9f4b170f7ea252ade9d5
VjiaoBlack <VjiaoBlack@gmail.com>
2014-07-30 07:53:26 +0000

    reverted back to using attr_accessible in users.rb model; also, added timek/pids and fixed around the unicorn and nginx config files so that nginx and unicorn both (seem to ) work.

--- timek/app/models/user.rb
+++ timek/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :email
 
-
+  attr_accessible :name, :email, :password, :password_confirmation
   has_many :schedules
   has_many :circles
   has_many :blocks, :through => :schedules
