c495554f86a177a5c7060dcdfbfafe493f3af21c
VjiaoBlack <VjiaoBlack@gmail.com>
2014-08-19 14:31:36 +0000

    added new stuff and organized nginx (hidden)

--- blog/config/unicorn.rb
+++ blog/config/unicorn.rb
@@ -1,19 +1,19 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/var/www/rails/Agnok/timek"
+working_directory "/var/www/rails/Agnok/blog"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/rails/Agnok/timek/pids/unicorn.pid"
+pid "/var/www/rails/Agnok/blog/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/rails/Agnok/timek/log/unicorn.log"
-stdout_path "/var/www/rails/Agnok/timek/log/unicorn.log"
+stderr_path "/var/www/rails/Agnok/blog/log/unicorn.log"
+stdout_path "/var/www/rails/Agnok/blog/log/unicorn.log"
 
 # Unicorn socket
-listen "/tmp/unicorn.timek.sock"
+listen "/tmp/unicorn.blog.sock"
 # listen "/tmp/unicorn.myapp.sock"
 
 # Number of processes
--- games/config/unicorn.rb
+++ games/config/unicorn.rb
@@ -1,20 +1,20 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/var/www/rails/Agnok/timek"
+working_directory "/var/www/rails/Agnok/games"
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/rails/Agnok/timek/pids/unicorn.pid"
+pid "/var/www/rails/Agnok/games/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/rails/Agnok/timek/log/unicorn.log"
-stdout_path "/var/www/rails/Agnok/timek/log/unicorn.log"
+stderr_path "/var/www/rails/Agnok/games/log/unicorn.log"
+stdout_path "/var/www/rails/Agnok/games/log/unicorn.log"
 
 # Unicorn socket
-listen "/tmp/unicorn.timek.sock"
-# listen "/tmp/unicorn.myapp.sock"
+listen "/tmp/unicorn.games.sock"
+# listen "/tmp/unicorn.games.sock"
 
 # Number of processes
 # worker_processes 4
